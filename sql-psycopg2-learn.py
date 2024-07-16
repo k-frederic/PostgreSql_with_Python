9cffeb7fb399e7c1cca123120614ca9a30039966
Frederic Kombou Ntondele <thanxname@outlook.com>
2024-07-16 04:24:16 +0000

    initial commit

--- sql-psycopg2-learn.py
+++ sql-psycopg2-learn.py
@@ -18,10 +18,17 @@ cursor = connection.cursor()
 
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
-cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', [51])
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with "ArtistId"#51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" =%s' , [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
 
 # fetch the results (multiple)
-results = cursor.fetchall()
+# results = cursor.fetchall()
 
 # fetch the results (single)
 results = cursor.fetchone()

d62c51350d39de1214408472f167a3ff8ee7efe1
Frederic Kombou Ntondele <thanxname@outlook.com>
2024-07-17 04:35:43 +0000

    sql-orm  lesson

--- sql-expression.py
+++ sql-expression.py
@@ -47,11 +47,18 @@ with db.connect() as connection:
     
     #Query 3 - select only 'Queen'from the "Artist" table
     # select_query =artist_table.select().where(artist_table.c.Name == "Queen")
-
-    
     
     #Query 4 - select only by 'ArtistID' #51 from the "Artist" table
-    select_query =artist_table.select().where(artist_table.c.ArtistId == 51)
+    # select_query =artist_table.select().where(artist_table.c.ArtistId == 51)
+
+    #Query 5 - select only the albums with 'ArtistId #51 on the "Album" table
+    select_query = album_table.select().where(album_table.c.ArtistId == 51)
+
+    #Query 6 - select all tracks where the composer is 'Queen' from the 'Track' table
+    # select_query = track_table.select().where(track_table.c.Composer == "Queen")
+
+
+
 
 
 
